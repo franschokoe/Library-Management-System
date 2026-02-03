4a0d8442767f07c80c2792bade28b0f6e2d965d1
Frans Chokoe <fmoyagabo@gmail.com>
2026-02-03 15:45:14 +0200

    araays

--- array.cpp
+++ array.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main (){
     int numberArray[5] = {1,2,3,4,5};
 
-    for (int k ; k < numberArray[5]; k++ ){
+    for(int k ; k < numberArray[5]; k++ ){
         
         cout<<k<<endl;
     }
