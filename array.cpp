5c25eef34b5a5ccf9228d88bf0342828c307b401
Frans Chokoe <fmoyagabo@gmail.com>
2026-02-03 16:27:32 +0200

    Complete 2d arrays

--- array.cpp
+++ array.cpp
@@ -1,17 +1,44 @@
 #include <iostream>
+#include <vector>
 #include <array>
 using namespace std;
 
 int main (){
     // normal arrays
     int numberArray[5] = {1,2,3,4,5};
+    vector<int> vectorArray = {1,2,4};
     int total= 0;
+    int total2 = 0;
+    //  for loop for first array
     for(int k = 0 ; k <5; k++ ){
         
         total += numberArray[k];
-
+        // when you want to see each step of iteration
+        // cout<<"The total is " << total << endl;
     }
     cout<<"The total is " << total << endl;
-    
+    // cout << "Vecto array is flexible "<< vectorArray<<endl;
+
+    // 2D arrays is like a matrix
+    // arrayDim[number of rows][number of columns]
+    int arrayDim[2][3] = {
+        {1,2,3},
+        {4,3,2}
+    };
+
+    // for loop for array 2d 
+    for(int i=0 ; i<2;i++){
+        for (int k = 0 ; k <3;k++){
+            // outputing array
+
+            cout<<arrayDim[i][k]<<endl;
+        }
+    }
+
+
+    cout<<"A new matrix complete"<<endl;
+
     cout<<"Complete iteration"<<endl;
+
+    return 0;
 }
