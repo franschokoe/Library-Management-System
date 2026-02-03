69c99f5a83f9a413e421afd52e3a594bba5ecbe4
Frans Chokoe <fmoyagabo@gmail.com>
2026-02-03 16:02:42 +0200

    Next commit of dummmy

--- array.cpp
+++ array.cpp
@@ -3,12 +3,15 @@
 using namespace std;
 
 int main (){
+    // normal arrays
     int numberArray[5] = {1,2,3,4,5};
-
-    for(int k ; k < numberArray[5]; k++ ){
+    int total= 0;
+    for(int k = 0 ; k <5; k++ ){
         
-        cout<<k<<endl;
-    }
+        total += numberArray[k];
 
+    }
+    cout<<"The total is " << total << endl;
+    
     cout<<"Complete iteration"<<endl;
 }
