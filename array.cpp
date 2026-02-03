66e9aa251dd6d41959e4fca1e019affd493be458
Frans Chokoe <fmoyagabo@gmail.com>
2026-02-03 15:33:55 +0200

    arrays

---
+++ array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main (){
+    int numberArray[5] = {1,2,3,4,5};
+
+    for (int k ; k < numberArray[5]; k++ ){
+        
+        cout<<k<<endl;
+    }
+
+    cout<<"Complete iteration"<<endl;
+}
